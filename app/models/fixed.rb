c7203758c888728914b4dd04237b031b3e8acc26
Aaron Lista <aarondavidlista@gmail.com>
2024-03-06 13:52:56 -0400

    validaciones corregidas

--- app/models/fixed.rb
+++ app/models/fixed.rb
@@ -1,6 +1,6 @@
 class Fixed < ApplicationRecord
   belongs_to :account
 
-  validates :amount, presence: true, comparison: { greater_than: 0 }
+  validates :amount, presence: true
   validates :category, presence: true
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -2,6 +2,6 @@ class Record < ApplicationRecord
   belongs_to :account
   belongs_to :plan
 
-  validates :amount, presence: true, comparison: { greater_than: 0 }
+  validates :amount, presence: true
   validates :category, :note, presence: true
 end
