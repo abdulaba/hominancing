1fc0158b3ff2ba95fa4777404c5fa98a91497d65
Aaron Lista <aarondavidlista@gmail.com>
2024-03-07 16:26:48 -0400

    validaciones fechas frontend hechas

---
+++ app/javascript/controllers/date_search_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="date-search"
+export default class extends Controller {
+  static targets = ["startDate", "endDate"]
+
+  connect() {
+    console.log(this.startDateTarget, this.endDateTarget)
+  }
+
+  setMin() {
+    this.endDateTarget.setAttribute("min", this.startDateTarget.value)
+  }
+
+  setMax() {
+    this.startDateTarget.setAttribute("max", this.endDateTarget.value)
+  }
+}
