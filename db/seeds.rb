ad553b6e2a4e91f6ce5909a474b920e199867740
Jose Peralta <joseperalta2910@gmail.com>
2024-03-08 17:00:11 -0400

    Categories agregadas, fechas aleatorias agregadas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
 Account.destroy_all
 Plan.destroy_all
 Fixed.destroy_all
@@ -16,6 +7,8 @@ COLORS = %w[#670f22 #9a0526 #E20000 #ff4040 #FF7676
         #005200 #007B00 #258D19 #4EA93B #588100]
 
 ACCOUNTS = %w[bancamiga mercantil banplus banesco efectivo]
+
+CATEGORIES = %w[comida transporte servicios ropa otros]
 
 aaron = User.find_or_initialize_by(nickname: "Aaron", email: "aarondlista@gmail.com")
 unless User.exists?(aaron.id)
@@ -44,7 +37,7 @@ users.each do |user|
   puts "creando cuentas para usuario"
 
   ACCOUNTS.each do |acct|
-    account = Account.new(name: acct.capitalize, balance: 10_000, color: COLORS.sample)
+    account = Account.new(name: acct.capitalize, balance: 0, color: COLORS.sample)
     account.user = user
     account.save
     puts "cuentas creadas para #{user.email}:\n nombre: #{account.name}, balance: #{account.balance}"
@@ -52,14 +45,22 @@ users.each do |user|
 
   puts "creando registros de la cuenta"
 
-  60.times do
-    record = Record.new(category: 0, note: "ejemplo")
-    record.amount = (100..500).to_a.sample
-    record.income = Random.rand(2) == 1
-    record.account = user.accounts.sample
-    record.account.balance += record.income ? record.amount : -record.amount
-    record.result = record.account.balance
-    record.account.save
-    record.save
+  (DateTime.new(2024, 1, 1)..DateTime.now).to_a.each do |date|
+    if [true, false].sample
+      (0..5).to_a.sample.times do
+        record = Record.new(category: 0, note: "ejemplo")
+        record.amount = (100..500).to_a.sample
+        record.income = Random.rand(2) == 1
+        record.account = user.accounts.sample
+        record.account.balance += record.income ? record.amount : -record.amount
+        record.result = record.account.balance
+        record.category = CATEGORIES.sample
+        record.created_at = date
+        record.account.save
+        record.save
+      end
+    end
   end
+
+
 end
