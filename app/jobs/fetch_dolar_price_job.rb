2f3c09eb70ff40ef56acf87f06bfa93227fda5b1
Jose Peralta <joseperalta2910@gmail.com>
2024-03-20 01:34:41 -0400

    Dolar resuelto

--- app/jobs/fetch_dolar_price_job.rb
+++ app/jobs/fetch_dolar_price_job.rb
@@ -5,7 +5,7 @@ class FetchDolarPriceJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    url = "https://pydolarvenezuela-api.vercel.app/api/v1/dollar?page=bcv&monitor=usd"
+    url = "https://pydolarvenezuela-api.vercel.app/api/v1/dollar/page?page=bcv&monitor=usd"
     res_serialized = URI.open(url).read
     res = JSON.parse(res_serialized)
 
@@ -14,9 +14,11 @@ class FetchDolarPriceJob < ApplicationJob
 
     date = DateTime.tomorrow
     next_date = DateTime.new(date.year, date.month, date.day, 8)
-    if info["price"] == price.price && info["price"].nil?
+    if info["price"] == price.price
       next_date = DateTime.now
       FetchDolarPriceJob.set(wait_until: next_date + 30.minutes).perform_later
+    else
+      price.update(price: info["price"], old_price: price.price)
     end
   end
 end
